429da65f682afacf0712839e2904bcd6652d2813
Ivan Vergés <ivan@pokecode.net>
2024-04-11 13:43:39 +0200

    sets the condition to show the login screen in the view (#27)

--- app/overrides/decidim/devise/sessions/new/replace_signup.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-Deface::Override.new(virtual_path: "decidim/devise/sessions/new",
-                     name: "replace-signup",
-                     replace_contents: ".wrapper",
-                     text: '<%= render "decidim/trusted_ids/devise/sessions/new" %>',
-                     disabled: !Decidim::TrustedIds.custom_login_screen?)
